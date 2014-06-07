4cc7ea3bc1e1a1e886c905a575591364b2acefdc
cjpalm <curtis.palm@qiagen.com>
2014-06-07 14:12:38 -0700

    plot2 initial commit

---
+++ plot2.R
@@ -0,0 +1,19 @@
+
+# load data file
+power.data <-read.csv2("./household_power_consumption.txt",na.strings="?")
+#pull out 2 dates of interest
+feb1 <- power.data[(as.Date(strptime(power.data$Date,"%d/%m/%Y")) - as.Date("2007-02-01") == 0),]
+feb2 <- power.data[(as.Date(strptime(power.data$Date,"%d/%m/%Y")) - as.Date("2007-02-02") == 0),]
+feb.1st.and.2nd.power.data <- rbind.data.frame(feb1,feb2)
+#draw graph on screen so can see how it looks.
+plot(as.numeric(as.character(feb.1st.and.2nd.power.data$Global_active_power)),
+     ylab="Global Active Power (kilowatts)",type="l", xlab="", xaxt="n")
+#add x-axis
+axis(at=c(1,1440,2880),side=1,labels=c("Thu","Fri","Sat"))
+#make graph as png file
+png("./figure/plot2.png",width = 480, height = 480)
+plot(as.numeric(as.character(feb.1st.and.2nd.power.data$Global_active_power)),
+     ylab="Global Active Power (kilowatts)",type="l", xlab="", xaxt="n")
+#add x-axis
+axis(at=c(1,1440,2880),side=1,labels=c("Thu","Fri","Sat"))
+dev.off()
