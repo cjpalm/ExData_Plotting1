78c0b3c6bc3c0657a9ccbe372d4d9675561d2e66
cjpalm <curtis.palm@qiagen.com>
2014-06-08 12:03:02 -0700

    fix mistakenly saved code from plot2

--- plot1.R
+++ plot1.R
@@ -12,14 +12,10 @@ feb1 <- power.data[(as.Date(strptime(power.data$Date,"%d/%m/%Y")) - as.Date("200
 feb2 <- power.data[(as.Date(strptime(power.data$Date,"%d/%m/%Y")) - as.Date("2007-02-02") == 0),]
 feb.1st.and.2nd.power.data <- rbind.data.frame(feb1,feb2)
 #draw graph on screen so can see how it looks.
-plot(as.numeric(as.character(feb.1st.and.2nd.power.data$Global_active_power)),
-     ylab="Global Active Power (kilowatts)",type="l", xlab="", xaxt="n")
-#add x-axis
-axis(at=c(1,1440,2880),side=1,labels=c("Thu","Fri","Sat"))
-#make graph as png file, use default white background
-png("./figure/plot2.png",width = 480, height = 480)
-plot(as.numeric(as.character(feb.1st.and.2nd.power.data$Global_active_power)),
-     ylab="Global Active Power (kilowatts)",type="l", xlab="", xaxt="n")
-#add x-axis
-axis(at=c(1,1440,2880),side=1,labels=c("Thu","Fri","Sat"))
+hist(as.numeric(as.character(feb1.2.power.data$Global_active_power)),
+     col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+#make histagram as png file
+png("./figure/plot1.png",width = 480, height = 480)
+hist(as.numeric(as.character(feb1.2.power.data$Global_active_power)),
+     col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
